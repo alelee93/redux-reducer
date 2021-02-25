585ad4839b06915209d3c91803d2749835da64f8
alelee93 <leevargasale@gmail.com>
2021-02-24 22:34:05 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    state = {friends: []}
+
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return {state: {...state, friends: action.friends}}
+        case 'REMOVE_FRIEND': 
+            return {state}
+        default: return state
+    }
+
 }
